acd96b32c0a2393c1a74ef508744a6404d29299c
Raphael Nivault <raphael.nivault@epita.fr>
2022-09-26 10:39:51 +0200

    clean useless lines

--- internal/clients/taikun.go
+++ internal/clients/taikun.go
@@ -82,14 +82,6 @@ func TerraformSetupBuilder(version, providerSource, providerVersion string) terr
 			return ps, errors.Wrap(err, errUnmarshalCredentials)
 		}
 
-		// set environment variables for sensitive provider configuration
-		// Deprecated: In shared gRPC mode we do not support injecting
-		// credentials via the environment variables. You should specify
-		// credentials via the Terraform main.tf.json instead.
-		/*ps.Env = []string{
-			fmt.Sprintf("%s=%s", "HASHICUPS_USERNAME", taikunCreds["username"]),
-			fmt.Sprintf("%s=%s", "HASHICUPS_PASSWORD", taikunCreds["password"]),
-		}*/
 		// set credentials in Terraform provider configuration
 		ps.Configuration = map[string]interface{}{}
 		if v, ok := taikunCreds[taikunEmail]; ok {
