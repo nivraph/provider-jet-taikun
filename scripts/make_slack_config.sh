46adbb044347b04509b5d07058d14d87321452aa
alexis-de-carvalho <alexis.martins-de-carvalho@epita.fr>
2022-10-18 14:47:05 +0200

    Fixed alerting and access examples

--- scripts/make_slack_config.sh
+++ scripts/make_slack_config.sh
@@ -24,4 +24,4 @@ sed -i "s/SLACK_CONFIGURATION/$CP_SC_NAME/g" $DEST
 sed -i "s/ORGANIZATION_REF/$CP_SC_ORG/g" $DEST
 sed -i "s/CHANNEL/$CP_SC_CHANNEL/g" $DEST
 sed -i "s/TYPE/$CP_SC_TYPE/g" $DEST
-sed -i "s/URL/$CP_SC_URL/g" $DEST
+sed -i "s^URL^$CP_SC_URL^g" $DEST
